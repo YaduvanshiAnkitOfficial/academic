4acc7d88d91e536b04cda7b519fdcc7107e3738d
Yaduvanshi Ankit <leaderyaduvanshi@gmail.com>
2019-04-11 04:58:20 +0530

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -2,12 +2,8 @@
 $toEmail = "yaduvanshiankit@outlook.com";
 $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
 if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
-    // Set a 200 (okay) response code.
-    http_response_code(200);
     print "<p class='success' style='background-color: #0DADB7;'>Thank You! Your message has been sent.</p>";
 } else {
-    // Set a 500 (internal server error) response code.
-    http_response_code(500);
     print "<p class='error' style='background-color: #e60000;'>Oops! Something went wrong and we couldn't send your message.</p>";
 }
 ?>
